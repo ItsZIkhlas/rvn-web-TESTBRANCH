aeb9f40d02a79f7e8fdf481f00d4f4887ddfca3b
Zikhlas <zayd.zakariya1013@gmail.com>
2025-10-06 11:51:56 -0400

    Update site-header.tsx

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -12,6 +12,7 @@ interface Props {
 
 export function SiteHeader({ siteHeader, onRefresh }: Props) {
   const { sessions, refresh } = useSessions();
+  
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center justify-between gap-1 px-4 lg:gap-2 lg:px-6">
